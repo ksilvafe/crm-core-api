1acc8829157ee9ad3acd9fd2130bfa6f04f02421
ksilvafe <ferreirakevin325@gmail.com>
2025-10-24 12:24:28 -0300

    feat: add and fix biomejs

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './domain/user.service';
-import { UserController } from './http/user.controller';
-
+import { Module } from "@nestjs/common";
+import { UserService } from "./domain/user.service";
+import { UserController } from "./http/user.controller";
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService],
+	controllers: [UserController],
+	providers: [UserService],
 })
 export class UserModule {}
